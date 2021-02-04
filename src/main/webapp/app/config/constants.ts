dd14b2c6eb5f955ccf4010a6a32dde81a482468f
mohadese <mohadeseh74k@gmail.com>
2021-02-05 00:49:52 +0330

    suppress errors for print

--- src/main/webapp/app/config/constants.ts
+++ src/main/webapp/app/config/constants.ts
@@ -5,7 +5,7 @@ const config = {
 export default config;
 
 // export const SERVER_API_URL = process.env.SERVER_API_URL;
-export const SERVER_API_URL = 'http://fdd83a0aefb6.ngrok.io/';
+export const SERVER_API_URL = 'http://19c9047576fc.ngrok.io/';
 
 export const AUTHORITIES = {
   ADMIN: 'ROLE_ADMIN',
