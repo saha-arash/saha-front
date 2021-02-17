eac48035e10fb102562a56b3b5d4a665f6dafec1
mohadese <mohadeseh74k@gmail.com>
2021-02-18 01:24:43 +0330

    icons

--- src/main/webapp/app/shared/layout/menus/entities.tsx
+++ src/main/webapp/app/shared/layout/menus/entities.tsx
@@ -5,119 +5,120 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Translate, translate } from 'react-jhipster';
 import { NavLink as Link } from 'react-router-dom';
 import { NavDropdown } from './menu-components';
+import { faTable } from '@fortawesome/free-solid-svg-icons';
 
 export const EntitiesMenu = props => (
   <NavDropdown icon="th-list" name="موجودیت ها" id="entity-menu" style={{ maxHeight: '80vh', overflow: 'auto' }}>
-    {/* <MenuItem icon="asterisk" to="/yegan-code">
+    {/* <MenuItem icon={faTable} to="/yegan-code">
       <Translate contentKey="global.menu.entities.yeganCode" />
     </MenuItem> */}
-    <MenuItem icon="asterisk" to="/karbar">
+    <MenuItem icon={faTable} to="/karbar">
       کاربران
     </MenuItem>
-    <MenuItem icon="asterisk" to="/yegan">
+    <MenuItem icon={faTable} to="/yegan">
       یگان
     </MenuItem>
-    <MenuItem icon="asterisk" to="/yegan-type">
+    <MenuItem icon={faTable} to="/yegan-type">
       نوع یگان ها
     </MenuItem>
-    <MenuItem icon="asterisk" to="/semat">
+    <MenuItem icon={faTable} to="/semat">
       سمت ها
     </MenuItem>
-    <MenuItem icon="asterisk" to="/daraje">
+    <MenuItem icon={faTable} to="/daraje">
       درجه ها
     </MenuItem>
-    <MenuItem icon="asterisk" to="/mantaghe">
+    <MenuItem icon={faTable} to="/mantaghe">
       منطقه
     </MenuItem>
-    <MenuItem icon="asterisk" to="/ostan">
+    <MenuItem icon={faTable} to="/ostan">
       استان
     </MenuItem>
-    <MenuItem icon="asterisk" to="/shahr">
+    <MenuItem icon={faTable} to="/shahr">
       شهر
     </MenuItem>
-    {/* <MenuItem icon="asterisk" to="/payam">
+    {/* <MenuItem icon={faTable} to="/payam">
       <Translate contentKey="global.menu.entities.payam" />
     </MenuItem> */}
-    {/* <MenuItem icon="asterisk" to="/file-name">
+    {/* <MenuItem icon={faTable} to="/file-name">
       <Translate contentKey="global.menu.entities.fileName" />
     </MenuItem> */}
-    {/* <MenuItem icon="asterisk" to="/morkhasi">
+    {/* <MenuItem icon={faTable} to="/morkhasi">
       <Translate contentKey="global.menu.entities.morkhasi" />
     </MenuItem>
-    <MenuItem icon="asterisk" to="/dore">
+    <MenuItem icon={faTable} to="/dore">
       <Translate contentKey="global.menu.entities.dore" />
     </MenuItem>
-    <MenuItem icon="asterisk" to="/negahbani">
+    <MenuItem icon={faTable} to="/negahbani">
       <Translate contentKey="global.menu.entities.negahbani" />
     </MenuItem> */}
-    {/* <MenuItem icon="asterisk" to="/barge-mamooriat">
+    {/* <MenuItem icon={faTable} to="/barge-mamooriat">
       <Translate contentKey="global.menu.entities.bargeMamooriat" />
     </MenuItem> */}
-    {/* <MenuItem icon="asterisk" to="/hesab-resi">
+    {/* <MenuItem icon={faTable} to="/hesab-resi">
       <Translate contentKey="global.menu.entities.hesabResi" />
     </MenuItem>
-    <MenuItem icon="asterisk" to="/barname-hesab-resi">
+    <MenuItem icon={faTable} to="/barname-hesab-resi">
       <Translate contentKey="global.menu.entities.barnameHesabResi" />
     </MenuItem>
-     <MenuItem icon="asterisk" to="/file-barge-mamooriat">
+     <MenuItem icon={faTable} to="/file-barge-mamooriat">
       <Translate contentKey="global.menu.entities.fileBargeMamooriat" />
     </MenuItem>  */}
-    <MenuItem icon="asterisk" to="/niroo-code">
+    <MenuItem icon={faTable} to="/niroo-code">
       نیرو ها
     </MenuItem> 
-    {/* <MenuItem icon="asterisk" to="/file-hesab-resi">
+    {/* <MenuItem icon={faTable} to="/file-hesab-resi">
       <Translate contentKey="global.menu.entities.fileHesabResi" />
     </MenuItem> */}
-    {/* <MenuItem icon="asterisk" to="/file-gozaresh">
+    {/* <MenuItem icon={faTable} to="/file-gozaresh">
       <Translate contentKey="global.menu.entities.fileGozaresh" />
     </MenuItem> */}
 
-    {/* <MenuItem icon="asterisk" to="/gozaresh">
+    {/* <MenuItem icon={faTable} to="/gozaresh">
       <Translate contentKey="global.menu.entities.gozaresh" />
     </MenuItem> */}
-    <MenuItem icon="asterisk" to="/daraje">
+    <MenuItem icon={faTable} to="/daraje">
       <span>درجه</span>
     </MenuItem>
-    {/* <MenuItem icon="asterisk" to="/mohasebe-hazine-mamooriat">
+    {/* <MenuItem icon={faTable} to="/mohasebe-hazine-mamooriat">
     <span>هزینه ماموریت</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/dastoor-amal-ejra-e">
+    <MenuItem icon={faTable} to="/dastoor-amal-ejra-e">
       <span>دستور العمل اجرایی</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/gardeshkar-barname-hesabresi">
+    <MenuItem icon={faTable} to="/gardeshkar-barname-hesabresi">
     <span>گردش کار</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/madarek">
+    <MenuItem icon={faTable} to="/madarek">
     <span>مدارک حسابرسی</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/bilan-sal-ghabl">
+    <MenuItem icon={faTable} to="/bilan-sal-ghabl">
     <span>بیلان سال قبل</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/gozaresh-hozoor">
+    <MenuItem icon={faTable} to="/gozaresh-hozoor">
     <span>گزارش حضور</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/chekide-gardesh-kar">
+    <MenuItem icon={faTable} to="/chekide-gardesh-kar">
     <span>چکیده گردش کار</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/bilan-se-sal-ghabl">
+    <MenuItem icon={faTable} to="/bilan-se-sal-ghabl">
     <span>بیلان سه سال قبل</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/mosta-khreje">
+    <MenuItem icon={faTable} to="/mosta-khreje">
     <span>مستخرجه</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/rafe-iradat">
+    <MenuItem icon={faTable} to="/rafe-iradat">
     <span>رفع ایرادات</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/bank-etelaati">
+    <MenuItem icon={faTable} to="/bank-etelaati">
     <span>بانک اطلاعاتی</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/nameh">
+    <MenuItem icon={faTable} to="/nameh">
     <span>نامه ها</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/kholase-gozaresh">
+    <MenuItem icon={faTable} to="/kholase-gozaresh">
     <span>خلاصه گزارش</span>
     </MenuItem>
-    <MenuItem icon="asterisk" to="/gardesh-kar">
+    <MenuItem icon={faTable} to="/gardesh-kar">
     <span>گردش کار</span>
     </MenuItem> */}
     {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
